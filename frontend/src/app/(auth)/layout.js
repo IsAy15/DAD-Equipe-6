6dd7cec5bf7020ffe64c70793f491fa0659ca015
IsAy15 <40857644+IsAy15@users.noreply.github.com>
2025-06-17 15:49:49 +0200

    Ajout de la registration page et amélioration des layout
    - Added a new registration form component with validation for username, email, and password.
    - Updated the authentication layout to include appearance settings in the header.
    - Refactored login form to use new translation keys and improved link handling.
    - Introduced appearance settings for theme and language selection.
    - Enhanced navbar with mobile support and dynamic aside menu for better navigation.
    - Implemented infinite scroll functionality in the home page.
    - Updated global styles to include a new font and theme configurations.
    - Added clsx for conditional class handling in components.

--- frontend/src/app/(auth)/layout.js
+++ frontend/src/app/(auth)/layout.js
@@ -1,3 +1,11 @@
+import AppearanceSettings from "@/components/AppearanceSettings";
 export default async function AuthLayout({ children }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <header className="fixed w-full z-50">
+        <AppearanceSettings />
+      </header>
+      {children}
+    </div>
+  );
 }
