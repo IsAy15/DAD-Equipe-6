86cd902dc94928dab7367f1f1e0dbd1e37d47756
Ilyass1309 <ilyass.tran@gmail.com>
2025-06-19 11:34:22 +0200

    feat: add complete message-service with send, inbox, sent, delete and edit (3x/15min) features

--- Backend/message-service/index.js
+++ Backend/message-service/index.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
-
-const mongoose = require('mongoose');
 const express = require('express');
+const mongoose = require('mongoose');
+const messageRoutes = require('./src/routes/message.routes');
+require('dotenv').config();
 
 const app = express();
 const port = 3002;
-
 app.use(express.json());
+app.use('/messages', messageRoutes);
 
 mongoose
-.connect(process.env.MONGODB_URI)
-.then(() => {
-    console.log('Connected to MongoDB for Message Service');
+    .connect("mongodb://mongo-message:27017/messagedb")
+    .then(() => {
+        console.log("Connected to the database!");
 
-    app.listen(port, () => {
-        console.log('Message Service is running on port', port);
-    });
-})
-.catch(err => {
-    console.error('Error connecting to MongoDB for Message Service:', err);
+        app.listen(port, () => {
+            console.log(`Message service listening on port ${port}`)
+        })
+    })
+    .catch(err => {
+        console.error("Database connection error:", err);
 });
