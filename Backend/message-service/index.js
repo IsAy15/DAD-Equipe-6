4b6c8fd67241b830c9fa600b366a81321404ae69
IsAy15 <40857644+IsAy15@users.noreply.github.com>
2025-06-19 11:09:48 +0200

    Ajout de la gestion des CORS pour tous les services

--- Backend/message-service/index.js
+++ Backend/message-service/index.js
@@ -1,22 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const mongoose = require('mongoose');
-const express = require('express');
+const mongoose = require("mongoose");
+const express = require("express");
 
 const app = express();
+const cors = require("cors");
+app.use(cors());
 const port = 3002;
 
 app.use(express.json());
 
 mongoose
-.connect(process.env.MONGODB_URI)
-.then(() => {
-    console.log('Connected to MongoDB for Message Service');
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB for Message Service");
 
     app.listen(port, () => {
-        console.log('Message Service is running on port', port);
+      console.log("Message Service is running on port", port);
     });
-})
-.catch(err => {
-    console.error('Error connecting to MongoDB for Message Service:', err);
-});
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB for Message Service:", err);
+  });
