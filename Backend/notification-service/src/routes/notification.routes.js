43c1b04f2b2c45ab36882171fad68ed440be118f
Ilyass1309 <125394900+Ilyass1309@users.noreply.github.com>
2025-06-20 11:38:50 +0200

    feat : notification on post added for followers

--- Backend/notification-service/src/routes/notification.routes.js
+++ Backend/notification-service/src/routes/notification.routes.js
@@ -4,5 +4,9 @@ const controller = require('../controllers/notification.controller');
 
 router.post('/', controller.createNotification);
 router.get('/:userId', controller.getUserNotifications);
+router.get('/:userId/count', controller.getNotificationCount);
+router.delete('/:notificationId', controller.readAndDelete);
+router.delete('/all/:userId', controller.readAllAndDelete);
+router.post('/api/notifications/on-post-created', controller.onPostCreated);
 
 module.exports = router;
