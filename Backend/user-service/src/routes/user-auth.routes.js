bd024a77e94d5842102e46c0712331ae525cf9cf
Ilyass1309 <ilyass.tran@gmail.com>
2025-06-24 10:40:29 +0200

    feat: username search added

--- Backend/user-service/src/routes/user-auth.routes.js
+++ Backend/user-service/src/routes/user-auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const userAuthController = require('../controllers/user-auth.controller');
 const router = express.Router();
+const verifyJWT = require('../middlewares/verifyJWT');
 
 router.get('/auth-data', userAuthController.getAuthData);
 router.post('/', userAuthController.createUser);
