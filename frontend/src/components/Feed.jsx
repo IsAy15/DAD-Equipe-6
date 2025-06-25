c817d61c0a121b92deb4706436f035ece3360d7d
besson <jules.besson@viacesi.fr>
2025-06-25 22:20:59 +0200

    adding translations

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -1,18 +1,21 @@
 import Post from "@/components/Post";
+import CommentsExpander from "@/components/comment/CommentsExpander";
+import { useTranslations } from "next-intl";
 
 export default function Feed({ posts, loadingPosts }) {
+  const t = useTranslations("Feed");
+
   return (
     <div className="flex flex-col w-full gap-4">
       {loadingPosts ? (
-        <p className="text-center">Chargement des posts...</p>
+        <p className="text-center"> {t("loading")}</p>
       ) : posts && posts.length > 0 ? (
         posts.map((post) => <Post key={post._id} post={post} />)
       ) : (
         <div>
-          <p className="text-center text-gray-500">Aucun post pour le moment.</p>
+          <p className="text-center text-base-content/50"> {t("noPosts")}</p>
         </div>
       )}
-      
     </div>
   );
 }
