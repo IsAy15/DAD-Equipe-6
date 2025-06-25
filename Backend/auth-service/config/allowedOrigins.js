692e7406f46c84641a677e089a3100603512c949
TheoC <theo.cvg@gmail.com>
2025-06-25 21:24:20 +0200

    fix: from localhost:80 to localhost

--- Backend/auth-service/config/allowedOrigins.js
+++ Backend/auth-service/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3005',
-    'http://localhost:80',
+    'http://localhost',
     'http://51.44.6.79:8080',
     'http://notification-service:3004',
     'http://post-service:3003',
